716165cf84e8233db74b1afcaa4fdceb66a20c46
Abhishek Soni <abhisheksoni.as444@gmail.com>
2024-01-19 12:51:47 +0530

    background img fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,18 @@ export default {
   theme: {
     extend: {
       colors:{
-        red:"#FF0336",
-        heroBgColor:"#0C1114"
+        'red':"#FF0336",
+        'heroBgColor':"#0C1114"
       },
       backgroundImage:{
-        heroBg:"url('./src/Images/young-fitness-man-studio.jpg')",
-        aboutcardbg:"url('./src/Images/home-2/background.jpg')",
-        aboutcardbgcange:"url('./src/Images/home-2/info-card-bg.jpg')",
-        textbg:"url('./src/Images/who-we-are/title-bg.svg')",
-        ci1:"url('./src/Images/classes/cycling.jpg')",
-        bg2:"url('./src/Images/backgorund_image.jpg')",
-        BMIbg:"url('./src/Images/BMI/bmi.jpg')",
-        addpage:"url('./src/Images/pagesStart/background.jpg')",
+        'heroBg':"url('./src/Images/young-fitness-man-studio.jpg')",
+        'aboutcardbg':"url('./src/Images/home-2/background.jpg')",
+        'aboutcardbgcange':"url('./src/Images/home-2/info-card-bg.jpg')",
+        'textbg':"url('./src/Images/who-we-are/title-bg.svg')",
+        'ci1':"url('./src/Images/classes/cycling.jpg')",
+        'bg2':"url('./src/Images/backgorund_image.jpg')",
+        'BMIbg':"url('./src/Images/BMI/bmi.jpg')",
+        'addpage':"url('./src/Images/pagesStart/background.jpg')",
       }
     },
   },
