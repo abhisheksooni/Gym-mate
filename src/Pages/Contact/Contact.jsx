e43be49e63277948700251b01906ea016251bcf2
Abhishek Soni <83975832+abhisheksooni@users.noreply.github.com>
2024-02-23 17:54:55 +0000

    build: creacte a Countact us page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <section className='bg-bgColor1 pb-10'>
+    {/* Background img */}
+  <div className="bg-addpage bg-no-repeat  text-center">
+  <div className="  h-[22rem]  bg-bgColor1/70  ">    
+  <h1 className='pt-[16rem] text-[#fff] text-4xl font-semibold'>Contact Us</h1>   
+  </div>
+  </div>
+{/* More data */}
+<div>
+  
+</div>
+    </section>
   )
 }
 
