9d96f7598adf55dc1db46009f963b7161b8bbce5
abhisheksooni <abhisheksoni.as444@gmail.com>
2024-03-04 23:24:11 +0530

    image fix

--- src/components/YogaAdd/YogaAdd.jsx
+++ src/components/YogaAdd/YogaAdd.jsx
@@ -3,7 +3,7 @@ import Button from '../Button/Button'
 
 function YogaAdd({img="bg2", h, p, span, pp, className, btntext,to}) {
   return (
-    <div className={` ${"bg-"+img}  bg-no-repeat-y w-full  uppercase text-text1 flex flex-col items-center p-6 py-16 my-12 ${className}`}>
+    <div className={` ${"bg-"+img?"addbg2":""}  bg-no-repeat-y w-full  uppercase text-text1 flex flex-col items-center p-6 py-16 my-12 ${className}`}>
         <h2  className=' text-[36px] font-bold text-center '>{h ? h:"yoga classes this summer"+"."}</h2>
 
             <p className=' text-[34px] w-80 text-center text-text1 my-9 '>{p? p : "pay now and  get"}<span className=' text-gold'>{span? span :" 35% "}</span>{pp? pp :"discont"}</p>
