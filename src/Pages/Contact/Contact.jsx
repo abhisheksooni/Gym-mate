16e725847538c6170f40b128cd09f34d1648e9bd
Abhishek Soni <abhisheksoni.as444@gmail.com>
2024-06-01 00:25:55 -0700

    fix map err

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Facebook,  Instagram, Twitter, Youtube } from 'lucide-react';
-import googleMapReact from 'google-map-react';
+
 
 function Contact() {
 
